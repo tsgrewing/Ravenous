bb60aaec0dcd88a48b0412550f31881a3aa79c42
Tony G <63308979+tsgrewing@users.noreply.github.com>
2021-05-04 00:28:30 -0500

    exporting business component

--- src/components/Business/business.js
+++ src/components/Business/business.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Business.css';
 
 const business = {
     imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
@@ -37,3 +38,4 @@ class Business extends React.Component {
     }
 };
 
+export default Business;
