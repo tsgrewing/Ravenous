71ffae8d5d8840317bd233d13279c36797fa8f11
Tony G <63308979+tsgrewing@users.noreply.github.com>
2021-05-12 13:29:04 -0500

    working properly

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,25 +2,22 @@ import React, { Component } from 'react';
 import './App.css';
 import SearchBar from '../SearchBar/SearchBar';
 import BusinessList from '../BusinessList/BusinessList';
-
-
-const business = {
-  imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
-  name: 'MarginOtto Pizzeria',
-  address: '1010 Paddington Way',
-  city: 'Flavortown',
-  state: 'NY',
-  zipCode: '10101',
-  category: 'Italian',
-  rating: 4.5,
-  reviewCount: 90
-};
-
-const businesses = [business, business, business, business, business, business]
+import Yelp from '../../util/Yelp';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      businesses: []
+    }
+    this.searchYelp = this.searchYelp.bind(this);
+  }
+
   searchYelp(term, location, sortBy) {
-    console.log(`searching for ${term} in ${location}, and sorting by ${sortBy}!`)
+    Yelp.search(term, location, sortBy)
+      .then(businesses => {
+        this.setState({businesses: businesses})
+      })
   }
 
   render() {
@@ -29,7 +26,7 @@ class App extends Component {
         <h1>ravenous</h1>
         <SearchBar 
           searchYelp={this.searchYelp} />
-        <BusinessList businesses={businesses} />
+        <BusinessList businesses={this.state.businesses} />
       </div>
     );
   }
--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,29 +1,32 @@
 const apiKey = "DGasfpW29DdXzsW6fVb1OCC_hroakMrz-uXYUnbPohhuNLCmCWz0KotFXYTlcU50nE-hACRCb83wDUbI9yGLZliNABQLkG3sScpfKCFi96nwjhsuoPN2k7puahacYHYx";
-const Yelp = {};
+const Yelp = {
 
-function search(term, location, sortBy) {
-    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {headers: {Authorization: `Bearer ${apiKey}`}})
-    .then(response => {return response.json()})
-    .then(jsonResponse => {
-        if (jsonResponse.businesses) {
-            return jsonResponse.businesses.map(business => {
-                let categories = [];
-                business.categories.forEach(cat => {
-                    categories.push(cat.title)
+    search(term, location, sortBy) {
+        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {headers: {Authorization: `Bearer ${apiKey}`}})
+        .then(response => {return response.json()})
+        .then(jsonResponse => {
+            if (jsonResponse.businesses) {
+                return jsonResponse.businesses.map(business => {
+                    let categories = [];
+                    business.categories.forEach(cat => {
+                        categories.push(cat.title)
+                    })
+                    return {
+                        id: business.id,
+                        imgSrc: business.image_url,
+                        name: business.name,
+                        address: business.location.address1,
+                        city: business.location.city,
+                        state: business.location.state,
+                        zipCode: business.location.zip_code,
+                        category: categories.join(', '),
+                        rating: business.rating,
+                        reviewCount: business.review_count 
+                    }
                 })
-                return {
-                    id: business.id,
-                    imageSrc: business.image_url,
-                    name: business.name,
-                    address: business.location.address1,
-                    city: business.location.city,
-                    state: business.location.state,
-                    zipCode: business.location.zip_code,
-                    category: categories.join(', '),
-                    rating: business.rating,
-                    reviewCount: business.review_count 
-                }
-            })
-        }
-    })
-}
+            }
+        })
+    }
+};
+
+export default Yelp;
